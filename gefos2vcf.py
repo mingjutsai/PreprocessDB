f8c1c1fd8b14ca9e8d0119e9fabcc58c68474a96
Ming-Ju <milutsai.bi98g@g2.nctu.edu.tw>
2019-09-29 11:47:33 -0400

    update function

--- gefos2vcf.py
+++ gefos2vcf.py
@@ -1,37 +1,42 @@
-# include standard modules
 import argparse
 import os
-import sys
 # initiate the parser
-parser = argparse.ArgumentParser()
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog='Transfer Gefos data to vcf format.',
+                                        description='')
 
-# add long and short argument
-parser.add_argument("--input", "-i", type=str, help="set output width")
+    # add long and short argument
+    parser.add_argument('input', type=str, nargs=1,
+                        help='Path for Gefos file.')
+    
+    # read arguments from the command line
+    args = parser.parse_args()
+    return args
 
-# read arguments from the command line
-args = parser.parse_args()
-
-# check for --width
-if args.input:
-    if os.path.exists(args.input):
-        gefos = args.input
+def main():
+    args = parse_arguments()
+    if os.path.exists(args.input[0]):
+        gefos = args.input[0]
         print('#gefos:' + gefos)
-else:
-    print('Need input file')
-print('#CHROM\tPOS\tID\tREF\tALT\tFILTER\tINFO\tFORMAT\tGEFOS')
-file = open(gefos,"r")
-for line in file:
-    clean_line = line.rstrip('\r\n')
-    fields = clean_line.split("\t")
-    rsid = fields[1]
-    if rsid == 'RSID':
-        continue
-    chr = fields[2]
-    pos = fields[3]
-    ref = fields[4]
-    alt = fields[5]
-    filter = '.'
-    info = fields[6] + ',' + fields[7] + ',' + fields[8] + ',' + fields[9] \
+    else:
+        raise Exception('input file does not exist')
+    print('#CHROM\tPOS\tID\tREF\tALT\tFILTER\tINFO\tFORMAT\tGEFOS')
+    file = open(gefos,"r")
+    for line in file:
+        clean_line = line.rstrip('\r\n')
+        fields = clean_line.split("\t")
+        rsid = fields[1]
+        if rsid == 'RSID':
+            continue
+        chr = fields[2]
+        pos = fields[3]
+        ref = fields[4]
+        alt = fields[5]
+        filter = '.'
+        info = fields[6] + ',' + fields[7] + ',' + fields[8] + ',' + fields[9] \
          + ',' + fields[10] + ',' + fields[11] + ',' + fields[12] + ',' + fields[13] 
-    print(chr + '\t' + pos + '\t' + rsid + '\t' + ref + '\t' + alt + '\t' + filter + '\t' + info + '\tNA\tNA')
-file.close()
+        print(chr + '\t' + pos + '\t' + rsid + '\t' + ref + '\t' + alt + '\t' + filter + '\t' + info + '\tNA\tNA')
+    file.close()
+
+if __name__ == "__main__":
+    main()
