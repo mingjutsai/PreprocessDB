2f43559c5ff4f6a804d2e23b46286605f01a28b9
Ming-Ju <milutsai.bi98g@g2.nctu.edu.tw>
2019-09-30 02:19:14 -0400

    add dbSNP process

---
+++ dbSNP/dbSNP2Annovar.py
@@ -0,0 +1,78 @@
+import os
+import argparse
+import re
+def parse_arguments():
+    parser = argparse.ArgumentParser(prog='Transfer dbSNP_vcf to Annovar format.',
+                                        description='')
+
+    # add long and short argument
+    parser.add_argument('dbsnp', type=str, nargs=1,
+                        help='Path for dbSNP_vcf file.')
+    parser.add_argument('assembly', type=str, nargs=1,
+                        help='Path for assembly_report.txt.')
+    # read arguments from the command line
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = parse_arguments()
+    if os.path.exists(args.dbsnp[0]):
+        dbsnp = args.dbsnp[0]
+        print('#dbSNP:' + dbsnp)
+    else:
+        raise Exception('dbSNP does not exist')
+    if os.path.exists(args.assembly[0]):
+        assembly = args.assembly[0]
+        print('#assembly:' + assembly)
+    else:
+        raise Exception('assembly does not exist')
+    #mapping refseq and ucsc
+    file = open(assembly,'r')
+    assembly_map = dict()
+    for line in file:
+        clean_line = line.rstrip('\r\n')
+        fields = clean_line.split("\t")
+        #fields[9]: UCSC-style-name
+        if fields[0] in ('1','2','3','4','5','6','7','8','9','10','11'
+                        ,'12','13','14','15','16','17','18','19','20'
+                        ,'21','22','X','Y','MT'):
+            refseq = fields[6]
+            ucsc = fields[0]
+            assembly_map[refseq] = ucsc
+            #print('refseq:' + refseq)
+            #print(assembly_map[refseq])
+        else:
+            continue
+    file.close
+
+    #read
+    rfile = open(dbsnp,'r')
+    output = dbsnp + '_annovar'
+    wfile = open(output,'w')
+    wfile.write('#Chr\tStart\tEnd\tRef\tAlt\tRSID\n')
+    for line in rfile:
+        clean_line = line.rstrip('\r\n')
+        fields = clean_line.split('\t')
+        match_header = re.match('^#',fields[0])
+        if match_header:
+            continue
+        else:
+            chr = assembly_map[fields[0]]
+            start = int(fields[1])
+            rsid = fields[2]
+            ref = fields[3]
+            alt = fields[4]
+            end = start + len(ref) - 1
+            if ',' in alt:
+                all_alt = alt.split(',')
+                for i in all_alt:
+                   wfile.write(chr + '\t' + str(start) + '\t' + str(end) + '\t' + ref + '\t' + i + '\t' + rsid + '\n')
+            else:
+                wfile.write(chr + '\t' + str(start) + '\t' + str(end) + '\t' + ref + '\t' + alt + '\t' + rsid + '\n')
+    rfile.close
+    wfile.close
+
+    
+
+if __name__ == '__main__':
+    main()
