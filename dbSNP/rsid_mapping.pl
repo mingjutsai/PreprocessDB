285e0e89898b9296a0f37b070e8e257d5e393a48
Ming-Ju <milutsai.bi98g@g2.nctu.edu.tw>
2019-11-26 07:46:42 +0000

    update rsid mapping

--- dbSNP/rsid_mapping.pl
+++ dbSNP/rsid_mapping.pl
@@ -2,8 +2,11 @@
 use strict;
 use warnings;
 my $dbsnp = $ARGV[0];
-my $input1 = $ARGV[1];
-my $input2 = $ARGV[2];
+my $input1 = $ARGV[1];#vcf format
+my $input2 = $ARGV[2];#vcf format
+if(@ARGV < 3){
+    print STDERR "perl rsid_mapping.pl dbSNP vcf1 vcf2\n";die;
+}
 my %rsid;
 open DBSNP,"<",$dbsnp;
 my $line=<DBSNP>;
@@ -15,7 +18,8 @@ while($line=<DBSNP>){
 }
 close DBSNP;
 
-my $out1 = $input1."_hg38_mapping_txt";
+#vcf format
+my $out1 = $input1."_hg38_mapping.vcf";
 open OUT1,">",$out1;
 open IN1,"<",$input1;
 $line=<IN1>;
@@ -23,16 +27,16 @@ print OUT1 $line;
 while($line=<IN1>){
     chomp $line;
     my @ele = split(/\t/,$line);
-    my $rsid = $ele[5];
+    my $rsid = $ele[2];
     if($rsid{$rsid}){
         my $pos = $rsid{$rsid};
-        print OUT1 $pos."\t".$ele[6]."\t".$ele[7]."\t".$ele[8]."\t".$ele[9]."\n";
+        print OUT1 $pos."\t".$ele[5]."\t".$ele[6]."\t".$ele[7]."\t".$ele[8]."\n";
     }
 }
 close IN1;
 close OUT1;
 
-my $out2 = $input2."_hg38_mapping.txt";
+my $out2 = $input2."_hg38_mapping.vcf";
 open OUT2,">",$out2;
 open IN2,"<",$input2;
 $line=<IN2>;
@@ -40,10 +44,10 @@ print OUT2 $line;
 while($line=<IN2>){
     chomp $line;
     my @ele = split(/\t/,$line);
-    my $rsid = $ele[5];
+    my $rsid = $ele[2];
     if($rsid{$rsid}){
         my $pos = $rsid{$rsid};
-        print OUT2 $pos."\t".$ele[6]."\t".$ele[7]."\t".$ele[8]."\t".$ele[9]."\n";
+        print OUT2 $pos."\t".$ele[5]."\t".$ele[6]."\t".$ele[7]."\t".$ele[8]."\n";
     }
 }
 close IN2;
