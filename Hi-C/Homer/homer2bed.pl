2829d9637cfcafbec59f515c9a78f9a8e4331778
milutsai <milutsai.bi98g@g2.nctu.edu.tw>
2020-11-30 11:42:10 +0800

    gothic2bed

--- Hi-C/Homer/homer2bed.pl
+++ Hi-C/Homer/homer2bed.pl
@@ -1,14 +1,23 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Number::FormatEng qw(:all);
+use bignum;
+use Statistics::Multtest qw(bonferroni holm hommel hochberg BH BY qvalue);
+use Statistics::Multtest qw(:all);
+
 my $homer_inter = $ARGV[0];
 my $fdr_cutoff = $ARGV[1];
 if(@ARGV<2){
     print STDERR "perl program Homer_SigInteraction fdr_cutoff\n";die;
 }
+my @pvalue;
+my $no = 1;
+my %loc1;
+my %loc2;
 my $origin_no = 0;
 my $filter_no = 0;
-my $output_bed = $homer_inter."_peak1_peak2.bed";
+my $output_bed = $homer_inter."_peak1_peak2_FDR".$fdr_cutoff.".bed";
 open OUT,">",$output_bed;
 open INTER,"<",$homer_inter;
 my $line=<INTER>;
@@ -17,10 +26,10 @@ while($line=<INTER>){
     $origin_no++;
     my @ele = split(/\t/,$line);
     my $fdr = $ele[18];
-    if($fdr > $fdr_cutoff){
-        next;
-    }
-    $filter_no++;
+    #if($fdr > $fdr_cutoff){
+    #    next;
+    #}
+    #$filter_no++;
     #my $id = $ele[0];
     my $chr_peak1 = $ele[2];
     my $start_peak1 = $ele[3];
@@ -32,14 +41,49 @@ while($line=<INTER>){
     my $end_peak2 = $ele[10];
     my $peak2_id = $chr_peak2.":".$start_peak2.":".$end_peak2;
     my $id = $chr_peak1.":".$start_peak1.":".$end_peak1."-".$chr_peak2.":".$start_peak2.":".$end_peak2;
+    #my $qvalue = format_eng($ele[18]);
+    my $pvalue = sprintf("%.2e",exp $ele[17]);
+    $pvalue[$no] = $pvalue;
+    #print STDERR "logP:".$ele[17]."\tP:".$pvalue."\n";die;
+    #print STDERR $ele[18]."\t".$qvalue."\t".format_eng($ele[18])."\n";
+    #my $qvalue = sprintf("%.2e",$ele[18]).",".$pvalue;
+    #print STDERR "qvalue:".$qvalue."\n";
     #my $peak2_id = $id."_".$chr_peak2."_bin2";
-    my $info1 = "InteractionID:".$id.",InteractionReads:".$ele[14].",LogP:".$ele[17].",FDR:".$ele[18];
-    my $info2 = "InteractionID:".$id.",InteractionReads:".$ele[14].",LogP:".$ele[17].",FDR:".$ele[18];
-    print OUT $chr_peak1."\t".$start_peak1."\t".$end_peak1."\t".$peak1_id."\t".$peak2_id."\t".$info1."\n";
-    print OUT $chr_peak2."\t".$start_peak2."\t".$end_peak2."\t".$peak2_id."\t".$peak1_id."\t".$info2."\n";
+    my @readcount = split(/\./,$ele[14]);
+    my $info = "InteractionID:".$id."\tReadCounts:".$readcount[0];
+    #my $info2 = "InteractionID:".$id.",InteractionReads:".$ele[14].",LogP:".$ele[17].",FDR:".$ele[18];
+    $loc1{$no} = $chr_peak1."\t".$start_peak1."\t".$end_peak1."\t".$peak1_id."\t".$peak2_id."\t".$info;
+    $loc2{$no} = $chr_peak2."\t".$start_peak2."\t".$end_peak2."\t".$peak2_id."\t".$peak1_id."\t".$info;
+    #print OUT $chr_peak1."\t".$start_peak1."\t".$end_peak1."\t".$peak1_id."\t".$peak2_id."\t".$info."\n";
+    #print OUT $chr_peak2."\t".$start_peak2."\t".$end_peak2."\t".$peak2_id."\t".$peak1_id."\t".$info."\n";
+    $no++;
 }
 close INTER;
-close OUT;
+#close OUT;
 
+my %hic_filter;
+my $p_value = \@pvalue;
+my $res = BH($p_value);
+#my @res = BH($p_value);
+for(my $i=1;$i<@{$res};$i++){
+    my $qvalue = $res->[$i];
+    if($qvalue <= $fdr_cutoff){
+	if(!$loc1{$i}){
+	    print $i."\n";
+	}
+	my @loc1_info = split("\t",$loc1{$i});
+	my @read = split(/:/,$loc1_info[6]);
+	my $id = $loc1_info[5];
+	my $readcount = $read[1];
+	if($readcount >= 10){
+	    $hic_filter{$id}++;
+            print OUT $loc1{$i}."\tq-value:".$qvalue."\n";
+	    print OUT $loc2{$i}."\tq-value:".$qvalue."\n";
+        }
+    }
+}
+my @uniq_Hic_filter = keys %hic_filter;
+$filter_no = scalar @uniq_Hic_filter;
+#print join "\n", @$res;
 print STDERR "origin:".$origin_no."\n";
 print STDERR "filter:".$filter_no."\n";
---
+++ Hi-C/gothic/gothic2bed.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+my $input = $ARGV[0];
+my $res = $ARGV[1];
+if(@ARGV < 2){
+    print STDERR "perl gothic2bed.pl input res(ex: 2000)\n";die;
+}
+my $output = $input."_peak1_peak2_res.".$res.".bed";
+open OUT,">",$output;
+open IN,"<",$input;
+while(my $line=<IN>){
+    chomp $line;
+    my @ele = split(/\t/,$line);
+    my $chr_bin1 = $ele[0];
+    my $chr_bin2 = $ele[3];
+    $chr_bin1 =~ s/chr//;
+    $chr_bin2 =~ s/chr//;
+    if($chr_bin2 ne $chr_bin1){
+        next;
+    }
+    my $start_bin1 = $ele[1] + 0;
+    my $end_bin1 = $ele[2] + 0;
+    my $start_bin2 = $ele[4] + 0;
+    my $end_bin2 = $ele[5] + 0;
+    my $bin1_id = $chr_bin1.":".$start_bin1.":".$start_bin2;
+    my $bin2_id = $chr_bin2.":".$start_bin2.":".$start_bin1;
+    my $readcount = $ele[10];
+    my $qvalue = sprintf("%.2e",$ele[12]);
+    my $id = $chr_bin1.":".$start_bin1.":".$start_bin2;
+    print OUT $bin1_id."\t".$readcount."\t".$qvalue."\n";
+    print OUT $bin2_id."\t".$readcount."\t".$qvalue."\n";
+}
+close IN;
+close OUT;
